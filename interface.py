8d2a8728f0be73a37134c2778b83d551a4d59c2a
timauri <timaurileecarby@gmail.com>
2024-11-27 17:29:05 +0100

    basic functions added

--- interface.py
+++ interface.py
@@ -1,27 +1,13 @@
 import customtkinter
 import serial 
 import time
+import json
 
+comport = 'COM3'
+ser = serial.Serial(comport, baudrate=9600, timeout=1)
 
-#update with actual COM port once I know
-comport = 'COM7'
-#ser = serial.Serial(comport, baudrate=9600, timeout=1)
-
-#holds different arguments for each command 
-arguments_dict = {
-    'MOVE_REL_STEPS': ['steps', 'direction', [int, str]],
-    'MOVE_REL_ANGLE': ['angle', [float]],
-    'MOVE_CONTINUOUS': ['direction', [str]],
-    'MOVE_ABS_ANGLE': ['angle', [float]],
-    'MOVE_ABS_STEPS': ['step_pos', [int]],
-    'SET_POS_ZERO': [],
-    'GET_ANGLE': [],
-    'GET_STEP_POS': []
-}
-
-"""
 def await_response(timeout=60):
-   awaits an input from the user
+    """awaits an input from the user"""
     reply = None
     t0 = time.time()
     while True:
@@ -35,105 +21,82 @@ def await_response(timeout=60):
             return None
 
 def send_command(command, *args):
-    sends a command to the stepper motor
+    """sends a command to the stepper motor"""
     ser.write((';'.join([command] + [str(a) for a in args]) + '\n').encode('utf-8'))
-"""
+
+def validate(value):
+    """converts input to an integer"""
+    val = int(value)
+    #print ({f'input value: {val} and type: {type(val)}'})
+    return int(value)
 
 app = customtkinter.CTk()
-app.geometry = ('500x500')
+app.geometry = ('1000x1000')
 app.title('Stepper Motor Interface')
 
-step_var = customtkinter.StringVar()
-dir_var = customtkinter.StringVar()
+def current_position():
+    """ gets the current positional values of the motor and displays it"""
+    current_angle = send_command('GET_ANGLE')
+    ang_response = json.loads(await_response())
+    angle = ang_response["result"]["angle"]
+    current_step = send_command('GET_STEP_POS')
+    step_out_response = json.loads(await_response())
+    step_pos = step_out_response["result"]["step_pos"]
+
+    frame_label.configure(
+        text=f"Current Angle: {angle}°\nCurrent Absolute Step Position: {step_pos}"
+    )
+
+frame = customtkinter.CTkFrame(master=app, width=1500, height=1500)
+frame.pack()
+
+frame_label = customtkinter.CTkLabel(
+    frame, text="Loading current position..."
+)
+frame_label.pack()
+
+current_position()
+
+stop_button = customtkinter.CTkButton(app, text="STOP MOTOR", 
+                                      command=lambda: send_command('MOTOR_STOP'),
+                                      fg_color='red')
+stop_button.pack(padx=20, pady=20)
+
+position_zero_button = customtkinter.CTkButton(app, text="SET MOTOR POSITION TO ZERO", 
+                                               command=lambda: send_command('SET_POS_ZERO'),
+                                               fg_color='blue')
+position_zero_button.pack(padx=30)
+
+
+#backward and forward buttons 
+move_buttons_frame = customtkinter.CTkFrame(app)
+move_buttons_frame.pack(pady=20)
+
+forward_button = customtkinter.CTkButton(move_buttons_frame, text="Move 1\u00B0 forward", 
+                                         command=lambda: send_command('MOVE_REL_ANGLE', 1, 'F'),
+                                         fg_color='green', width=140)
+forward_button.pack(side='left', padx = 10)
+backward_button = customtkinter.CTkButton(move_buttons_frame, text="Move 1\u00B0 backward", 
+                                          command=lambda: send_command('MOVE_REL_ANGLE', 1, 'B'),
+                                          fg_color='green', width=140)
+backward_button.pack(side='left', padx = 10)
+
+#set angle buttons 
 angle_var = customtkinter.StringVar()
-step_pos_var = customtkinter.StringVar()
+set_angle_label = customtkinter.CTkLabel(app, text = 'Set Angle:')
+set_angle_label.pack(pady = 10)
+set_angle = customtkinter.CTkEntry(app, textvariable=angle_var)
+set_angle.pack(pady = 10)
 
-def send_command(command, *args):
-    """empty function for testing"""
-    pass 
+angle_buttons_frame = customtkinter.CTkFrame(app)
+angle_buttons_frame.pack(pady=20)
 
-def optionmenu_callback(choice):
-    """ defines what happens when a specific argument is chosen from the dropdown"""
-    if choice == 'SET_POS_ZERO' or choice == 'GET_ANGLE' or choice == 'GET_STEP_POS':
-        send_command(choice)
-        print (f'Sending command {choice} to stepper motor')
-    elif choice == 'MOVE_REL_ANGLE' or choice == 'MOVE_ABS_ANGLE':
-        angle = angle_var.get()
-        send_command(choice, angle)
-        print (f'Sending command {choice} to stepper motor')
-    elif choice == 'MOVE_REL_STEPS':
-        steps = step_var.get()
-        direction = dir_var.get()
-        send_command(choice, steps, direction)
-    elif choice == 'MOVE_CONTINUOUS': 
-        send_command(choice, direction)
-        print (f'Sending command {choice} to stepper motor')
-    else:
-        step_pos = step_pos_var.get()
-        send_command(choice, step_pos)               
-    
-    
-    """
-    if choice == 'MOVE_REL_STEPS':
-        send_command('MOVE_REL_STEPS', steps, direction)
-        print(f'Sending command {choice} to stepper motor')
-    elif choice == 'MOVE_REL_ANGLE':
-        send_command('MOVE_REL_ANGLE', angle)
-        print(f'Sending command {choice} to stepper motor')
-    else:
-        send_command('MOVE_ABS_ANGLE', angle)
-        print(f'Sending command {choice} to stepper motor')
-    """
-
-
-
-#command choices 
-optionmenu = customtkinter.CTkOptionMenu(app, values=["MOVE_REL_STEPS", 
-                                                      "MOVE_REL_ANGLE",
-                                                      "MOVE_CONTINUOUS",
-                                                      "MOVE_ABS_ANGLE",
-                                                      "MOVE_ABS_STEPS",
-                                                      "SET_POS_ZERO",
-                                                      "GET_ANGLE",
-                                                      "GET_STEP_POS"],
-                                         command=optionmenu_callback)
-optionmenu.set("Choose an option")
-optionmenu.grid(padx = 20, pady = 20, sticky = 'ew')
-
-
-# arguments display - need to have boxes stretch to display all text
-steps_entry = customtkinter.CTkEntry(app, textvariable=step_var)
-steps_label = customtkinter.CTkLabel(app, text = 'Steps')
-steps_label.grid(row = 1, column = 0)
-steps_entry.grid(row = 1, column = 1, sticky = 'ew')
-
-direction_entry = customtkinter.CTkEntry(app, textvariable=dir_var)
-direction_label = customtkinter.CTkLabel(app, text = 'Direction')
-direction_label.grid(row = 2, column = 0, ipadx = 20)
-direction_entry.grid(row = 2, column = 1, sticky = 'ew')
-
-angle_entry = customtkinter.CTkEntry(app, textvariable=angle_var)
-angle_label = customtkinter.CTkLabel(app, text = 'Angle')
-angle_label.grid(row = 3, column = 0)
-angle_entry.grid(row = 3, column = 1, sticky = 'ew')
-
-step_pos_entry = customtkinter.CTkEntry(app, textvariable=step_pos_var)
-step_pos_label = customtkinter.CTkLabel(app, text = 'Step Position')
-step_pos_label.grid(row = 4, column = 0)
-step_pos_entry.grid(row = 4, column = 1, sticky = 'ew')
-
-button = customtkinter.CTkButton(app, text="Enter")
-button.grid(padx = 10, pady = 10)
-
+rel_angle_button = customtkinter.CTkButton(angle_buttons_frame, text="Use relative angle", 
+                                           command=lambda: send_command('MOVE_REL_ANGLE', validate(angle_var.get())))
+abs_angle_button = customtkinter.CTkButton(angle_buttons_frame, text="Use absolute angle", 
+                                           command=lambda: send_command('MOVE_ABS_ANGLE', validate(angle_var.get())))
+rel_angle_button.pack(side='left', padx=10)
+abs_angle_button.pack(side='left', padx=10)
 
 app.mainloop()
-
-"""
-things to consider:
-- when telling it to move to a certain angle, won't move to actual angle unless it's a multiple of 180 degrees (move-rel-angle/move-abs-angle)
-- read in argument values from user 
-- check type of input to make sure only valid inputs are made
-- need an enter button 
-- want to show options after drop down option is selected 
-"""
 
